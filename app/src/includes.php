ca53378f880910966507aab436eba292e673a800
rudolf <rudolf@tygron.com>
2024-12-10 17:02:03 +0100

    Collective including

---
+++ app/src/includes.php
@@ -0,0 +1,11 @@
+<?php
+
+	include_once './src/AutoLoader/SimpleAutoLoader.php';
+	SimpleAutoloaderAddSourceDirectory( __DIR__ );
+
+	include_once 'config.php';
+	include_once 'input.php';
+	include_once 'texts.php';
+
+
+?>
