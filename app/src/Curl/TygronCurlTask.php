6d92bbf47fd3c988a9e289322089a3df51607b9d
rudolf <rudolf@tygron.com>
2024-12-10 16:58:05 +0100

    Curl wrapper for Tygron-specific calls

---
+++ app/src/Curl/TygronCurlTask.php
@@ -0,0 +1,68 @@
+<?php
+	namespace Curl;
+
+	class TygronCurlTask extends CurlTask {
+
+		public function setCredentials( string $username, string $password ) {
+			$auth = $username.':'.$password;
+
+			$b64Auth = base64_encode($auth);
+			return $this->setAuthHeader( $b64Auth, 'Basic' );
+		}
+
+		public function setToken( string $apiToken ) {
+			return $this->setParameters( ['token' => $apiToken] );
+		}
+
+		public function setUrl( string $platform = 'engine', string $path = '/' ) {
+			if ( str_contains($platform, '/') ) {
+				parent::setUrl($platform);
+			}
+			$url = 'https://'.$platform.'.tygron.com';
+			if ( !empty($path) ) {
+				if ( !str_starts_with($path,'/') ) {
+					$url = $url.'/';
+				}
+				$url = $url.$path;
+			}
+			return parent::setUrl($url);
+		}
+
+		public static function get( array|string $credentials, string $platform, string $path, string|array $data = null, bool $json = true ) {
+			$curlTask = new \Curl\TygronCurlTask();
+			$curlTask
+				->setMethod('GET')
+				->setUrl($platform, $path)
+				->setData($data);
+
+			if ($json) {
+				$curlTask->setParameters(['f'=>'JSON']);
+			}
+			if ( is_string($credentials) ) {
+				$curlTask->setParameters(['token'=>$credentials]);
+			} else if ( is_array($credentials) ) {
+				$curlTask->setCredentials($credentials['username'], $credentials['password']);
+			}
+			return $curlTask;
+		}
+
+                public static function post( array|string $credentials, string $platform, string $path, string|array $data = null, bool $json = true ) {
+                        $curlTask = new \Curl\TygronCurlTask();
+                        $curlTask
+				->setMethod('POST')
+                        	->setUrl($platform, $path)
+                        	->setData($data);
+
+			if ($json) {
+                        	$curlTask->setParameters(['f'=>'JSON']);
+			}
+                        if ( is_string($credentials) ) {
+                                $curlTask->setParameters(['token'=>$credentials]);
+                        } else if ( is_array($credentials) ) {
+                                $curlTask->setCredentials($credentials['username'],$credentials['password']);
+                        }
+			return $curlTask;
+                }
+
+	}
+?>
