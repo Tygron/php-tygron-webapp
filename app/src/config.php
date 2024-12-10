f2e6db00c28c9c509f9d61f09b7f86dcca237809
rudolf <rudolf@tygron.com>
2024-12-10 17:01:31 +0100

    Config file

---
+++ app/src/config.php
@@ -0,0 +1,12 @@
+<?php
+	$WORKSPACE_DIR = implode(DIRECTORY_SEPARATOR, ['','var','workspace']);
+
+	$WORKSPACE_TASK_DIR = implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'tasks']);
+	$WORKSPACE_CREDENTIALS_DIR = implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'credentials']);
+
+	$CREDENTIALS_FILE_DEFAULT = 'default-credentials.json';
+
+	$SAFE_CHARACTERS = '[a-zA-Z0-9\-_]';
+
+	$LANGUAGE_DEFAULT = 'EN'
+?>
