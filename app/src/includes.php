21d155c2d1ebd39075a1171b66d21e36050dc171
rudolf <rudolf@tygron.com>
2025-01-09 12:55:13 +0100

    More robust include statement for calls from other locations

--- app/src/includes.php
+++ app/src/includes.php
@@ -1,6 +1,6 @@
 <?php
 
-	include_once './src/AutoLoader/SimpleAutoLoader.php';
+	include_once __DIR__.DIRECTORY_SEPARATOR.'AutoLoader/SimpleAutoLoader.php';
 	SimpleAutoloaderAddSourceDirectory( __DIR__ );
 
 	include_once 'config.php';
