ba0a89dcabaeb038f945303f0d797b22e9804ace
rudolf <rudolf@tygron.com>
2025-01-20 11:35:08 +0100

    Check credentials are valid at start of process. Retain login key instead of raw password

---
+++ app/src/Operations/ValidateCredentialsFile.php
@@ -0,0 +1,43 @@
+<?php
+
+	namespace Operations;
+
+	class ValidateCredentialsFile {
+
+		public function __construct() {
+
+		}
+
+		public static function run($task) {
+			global $WORKSPACE_CREDENTIALS_DIR;
+
+			$credentialsFileName = $task->getCredentialsFileName();
+			if ($credentialsFileName == $task->getDefaultCredentialsFileName()) {
+				return true;
+			}
+
+			$credentials = \Tasks\Task::loadCredentials($credentialsFileName);
+			$curlTask = \Curl\TygronCurlTask::post($credentials, $credentials['platform'], 'api/event/user/get_my_login_key', [])->run();
+			if ( !$curlTask->getResponseIsSuccess() ) {
+				throw new \Exception('Credentials were invalid');
+			}
+			$task->log(get_text('Validated credentials for user'));
+
+			$credentials['password'] = $curlTask->getContent();
+
+                        \Utils\Files::WriteJsonFile([$WORKSPACE_CREDENTIALS_DIR, $credentialsFileName], $credentials);
+			return true;
+		}
+
+
+		public static function checkReadyForOperation($task) {
+			return !empty($task->getCredentialsFileName());
+		}
+
+		public static function checkOperationComplete($task, bool $thrown = true) {
+			return $task->getOperationResult() === true;
+		}
+	}
+
+
+?>
