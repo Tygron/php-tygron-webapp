fa88e0223c69ec39137fe1b6a212654d22813dc3
rudolf <rudolf@tygron.com>
2025-01-20 09:24:17 +0100

    Structuring of config and config override options

---
+++ app/config/sample-config.php
@@ -0,0 +1,16 @@
+<?php
+
+	//Location of the workspace folder
+	$WORKSPACE_DIR = '/var/workspace/';
+
+	//Authentication token. If set, this token must be provided before access to the application proper.
+	$AUTHENTICATION_TOKEN = null;
+
+	//The name of the default credentials file. When set, users needn't provide their own Tygron Platform credentials.
+	//Should be placed in the workspace folder, in a subfolder named "credentials".
+	$CREDENTIALS_FILE_DEFAULT = 'credentials-default.json';
+
+	//The (default) language to use for texts and messages.
+	$LANGUAGE_DEFAULT = 'EN';
+
+?>
--- app/src/config.php
+++ app/src/config.php
@@ -1,15 +1,21 @@
 <?php
-	$WORKSPACE_DIR = implode(DIRECTORY_SEPARATOR, ['','var','workspace']);
 
-	$WORKSPACE_TASK_DIR = implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'tasks']);
-	$WORKSPACE_CREDENTIALS_DIR = implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'credentials']);
+	$CONFIG_OVERRIDE_FILE = implode(DIRECTORY_SEPARATOR,[__DIR__,'..','config','config.php']);
+	if (file_exists($CONFIG_OVERRIDE_FILE)) {
+		include_once($CONFIG_OVERRIDE_FILE);
+	}
 
-	$CREDENTIALS_FILE_DEFAULT = 'credentials-default.json';
+	//Some of the variables below can be overwritten, others should not.
+	//To overwrite, find the "config" folder, create a copy of sample-config.php named config.php and make the desires changes there.
 
-	$SAFE_CHARACTERS = '[a-zA-Z0-9\-_]';
+	$WORKSPACE_DIR = $WORKSPACE_DIR		??	implode(DIRECTORY_SEPARATOR, ['','var','workspace']);
+	$WORKSPACE_TASK_DIR =				implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'tasks']);
+	$WORKSPACE_CREDENTIALS_DIR = 			implode(DIRECTORY_SEPARATOR, [$WORKSPACE_DIR, 'credentials']);
 
-	$DEFAULT_ACTION = 'CreateTaskForm';
-	$AUTHENTICATION_TOKEN = 'Welcome123';
+	$CREDENTIALS_FILE_DEFAULT = $CREDENTIALS_FILE_DEFAULT ?? 'credentials-default.json';
+	$LANGUAGE_DEFAULT = $DEFAULT_LANGUAGE 	?? 	'EN';
 
-	$LANGUAGE_DEFAULT = 'EN'
+
+	$SAFE_CHARACTERS = $SAFE_CHARACTER 	?? 	'[a-zA-Z0-9\-_]';
+	$DEFAULT_ACTION = $DEFAULT_ACTION 	?? 	'CreateTaskForm';
 ?>
