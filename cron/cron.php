430f9c3e762a9f572c1b45b2cf16c9f1ccafe81d
rudolf <rudolf@tygron.com>
2025-01-20 08:51:07 +0100

    Added initial versions of crojob-callable files

---
+++ cron/cron.php
@@ -0,0 +1,8 @@
+<?php
+
+	$documentroot = "/var/www/html";
+	include_once implode(DIRECTORY_SEPARATOR, [$documentroot, 'src','includes.php']);
+
+	$runner = new \Runners\TasksRunner();
+	$runner->run();
+?>
