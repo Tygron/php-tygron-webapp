aa2fe46146f43d49226c2cb3813f222f0c868eee
rudolf <rudolf@tygron.com>
2025-02-13 16:03:35 +0100

    Updated cron references to new Operations structure

--- app/application/cron.php
+++ app/application/cron.php
@@ -2,7 +2,7 @@
 
 	include_once "src/includes.php";
 
-	$tasksRunner = new \Runners\TasksRunner();
+	$tasksRunner = new \Tasks\Runners\TasksRunner();
 	$tasksRunner->run();
 
 ?>
--- cron/cron.php
+++ cron/cron.php
@@ -3,6 +3,6 @@
 	$documentroot = "/var/www/html";
 	include_once implode(DIRECTORY_SEPARATOR, [$documentroot, 'application','src','includes.php']);
 
-	$runner = new \Runners\TasksRunner();
+	$runner = new \Tasks\Runners\TasksRunner();
 	$runner->run();
 ?>
