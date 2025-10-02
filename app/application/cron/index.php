47ecf17f0e75aa92bef290b549637e3f17cf25eb
rudolf <rudolf@tygron.com>
2025-10-02 13:49:20 +0200

    Cron endpoint move, cli_token check clearer

---
+++ app/application/cron/index.php
@@ -0,0 +1,36 @@
+<?php
+
+	include_once "../src/includes.php";
+
+
+	if ( $RUNNING_FROM_CLI ) {
+	} else if ( is_null($CLI_TOKEN) ) {
+	} else if (  (!empty($CLI_TOKEN)) && (get_clean_user_input('cli_token') === $CLI_TOKEN) ) {
+	} else {
+		return;
+	}
+
+	if ( !empty($CRON_LAST_RUN_FILE) ) {
+		\Utils\Files::writeFile(
+				[$WORKSPACE_DIR, $CRON_LAST_RUN_FILE],
+				\Utils\Time::GetCurrentTimestamp()
+			);
+	}
+
+	foreach ( $CRON_TASKS as $index => $jobName) {
+		$filePath = $CUSTOM_CRON_DIR.DIRECTORY_SEPARATOR.$jobName;
+		if ( !file_exists($filePath) ) {
+			$filePath = $APP_CRON_DIR.DIRECTORY_SEPARATOR.$jobName;
+		}
+		if ( !file_exists($filePath) ) {
+			echo 'not found: '.$filePath;
+		}
+
+		echo get_text( 'Running crontask %s',[$jobName] );
+		echo '<hr>';
+		include_once($filePath);
+	}
+
+	//}
+
+?>
