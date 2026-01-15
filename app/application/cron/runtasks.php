3dcd49a03ed0ec3364226306fd2da3ae8b287600
rudolf <rudolf@tygron.com>
2026-01-15 12:03:32 +0100

    Output logs when runners called directly

--- app/application/cron/runtasks.php
+++ app/application/cron/runtasks.php
@@ -8,21 +8,34 @@
 		$runSpecificTask = get_clean_user_input('task');
 
 		if ($runSpecificTask) {
+			$taskRunner = new \Tasks\Runners\TaskRunner();
 			try {
-				$taskRunner = new \Tasks\Runners\TaskRunner();
 				$taskRunner->setTask($runSpecificTask);
 				$taskRunner->run();
+				foreach ($taskRunner->getLogs as $key => $value) {
+					log_message($value);
+				}
+
 			} catch ( \Throwable $e ) {
+				foreach ($taskRunner->getLogs as $key => $value) {
+					log_message($value);
+				}
                                 log_message(get_text('Encountered an error while running single task: %s',[$e->getMessage()]));
                                 log_message(get_text('Location: %s, Line: %s',[$e->getFile(),$e->getLine()]));
                                 log_message(get_text('Stacktrace: %s',['<pre>'.$e->getTraceAsString().'</pre>']));
 			}
 		} else {
+			$tasksRunner = new \Tasks\Runners\TasksRunner();
 			try {
-				$tasksRunner = new \Tasks\Runners\TasksRunner();
 				$tasksRunner->setStandOffTimeInSeconds( $TASKS_STANDOFF_IN_SECONDS );
 				$tasksRunner->run();
+				foreach ($tasksRunner->getLogs as $key => $value) {
+					log_message($value);
+				}
 			} catch ( \Throwable $e ) {
+				foreach ($tasksRunner->getLogs as $key => $value) {
+					log_message($value);
+				}
 				log_message(get_text('Encountered an error while running all tasks: %s',[$e->getMessage()]));
 				log_message(get_text('Location: %s, Line: %s',[$e->getFile(),$e->getLine()]));
 				log_message(get_text('Stacktrace: %s',['<pre>'.$e->getTraceAsString().'</pre>']));
