c6ce7bbdc83eee182711dc9cf15b78dd46c3f4b3
rudolf <rudolf@tygron.com>
2026-01-21 08:47:34 +0100

    Preparatory function split for more complex rendering

--- app/application/src/Rendering/Renderer.php
+++ app/application/src/Rendering/Renderer.php
@@ -20,21 +20,39 @@
 				return $asset;
 			}
 			foreach ( $data as $key => $value ) {
-				if (is_array($value)) {
-					$can_be_imploded = true;
-					foreach ($value as $subKey => $subValue) {
-						$can_be_imploded = false;
-						break;
-					}
-					$value = $can_be_imploded ? '['.implode(',',$value).']' : '('.get_text('multiple values').')';
-				}
-				$asset = preg_replace('/{{(\s)*'.$key.'(\s)*}}/',$value ?? '', $asset);
+
+				$asset = self::computeReplaceAsArray($asset, $key, $value);
+
+				$value = self::implodeDataIfPossible($value);
+				$asset = self::computeReplaceSimple($asset, $key, $value);
 			}
 			return $asset;
 		}
 
 		public static function clean( string $asset ) {
 			$asset = preg_replace('/{{(\s)*(.*)(\s)*}}/','',$asset);
+			return $asset;
+		}
+
+		private static function implodeDataIfPossible($value, string $default = 'multiple values') {
+			if (is_array($value)) {
+				$can_be_imploded = true;
+				foreach ($value as $subKey => $subValue) {
+					$can_be_imploded = false;
+					break;
+				}
+				$value = $can_be_imploded ? '['.implode(',',$value).']' : '('.get_text($default).')';
+			}
+			return $value;
+		}
+
+		private static function computeReplaceAsArray( $asset, $key, $value ) {
+			//{% for item in items %}{% include 'template_per_element.html' with { item: item } only %}{% endfor %}
+			return $asset;
+		}
+
+		private static function computeReplaceSimple( $asset, $key, $value ) {
+			$asset = preg_replace('/{{(\s)*'.$key.'(\s)*}}/', $value ?? '', $asset);
 			return $asset;
 		}
 
