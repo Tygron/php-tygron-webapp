557d478d3bc7a1e79ed1f710655611b2d5f983d1
rudolf <rudolf@tygron.com>
2026-05-21 11:03:26 +0200

    Remove workspace subdirs in cleanup

--- workspace/clean_workspace.sh
+++ workspace/clean_workspace.sh
@@ -1,3 +1,7 @@
 rm ./tasks/*-task.json
+rm ./tasks/*/*-task.json
+#Clear context directories, avoiding syntax which may delete targets of symlinks
+find ./tasks -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
+
 rm ./credentials/*-credentials.json
 rm ./credentials/transient/*.json
